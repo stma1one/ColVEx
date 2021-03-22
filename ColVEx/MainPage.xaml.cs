6e339ee2c7f3e6c39393460c970b8bb992df0463
Home <Home@LAPTOP-2E6V6ACM>
2021-03-22 17:50:13 +0200

    Add project files.

---
+++ ColVEx/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PageNavigationMonkeys;
+
+namespace ColVEx
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.BindingContext = new Monkeys();
+            InitializeComponent();
+        }
+    }
+}
